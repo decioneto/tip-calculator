9018d332565e942393fd5ab32de4f8f02897bd4b
decioneeto <decio.neto@tribofox.com.br>
2022-01-04 17:28:41 -0300

    new js file

--- app.js
+++ app.js
@@ -1,82 +1,34 @@
-const DOM = {
-
-  activeTipButton(){
-    const tipButtonsGrid = document.querySelector('.buttons-grid')
-    const tipButton = tipButtonsGrid.getElementsByClassName('tip-button') 
-
-    for(let i = 0; i < tipButton.length; i++) {
-      tipButton[i].addEventListener('click', () => {
-        const activeClass = document.getElementsByClassName('active')
-        if(activeClass.length > 0) {
-          activeClass[0].classList.remove('active')
-        }
-
-        tipButton[i].classList.add('active')
-      })
-    }
-  },
-
-  calculateTipAmount() {
-    const bill = Form.getBillValue();
-    const tip = Form.activeTipButtonValue();
-    const dividedBy = Form.getDivideByvalue();
-
-    const totalTipAmoute = bill * tip
-
-    console.log(totalTipAmoute)
-  },
-}
-
-const Utils = {
-  formatValue(value) {
-    value = String(value).replace(/\D/, '')
-    value = Number(value)
-    
-    return value
-  }
-}
-
 const Form = {
-
-  bill: document.getElementById('bill'),
-  dividedBy: document.getElementById('people'),
-  tipCustomValue: document.querySelector('#tipCustomValue'),
+  bill: document.querySelector('#bill'),
+  customTip: document.querySelector('#tipCustomValue'),
+  dividedBy: document.querySelector('#people'),
 
   getValues() {
     return {
       bill: Form.bill.value,
-      dividedBy: Form.dividedBy.value,
-      tipCustomValue: Form.tipCustomValue.value
+      customTip: Form.customTip.value,
+      dividedBy: Form.dividedBy.value
     }
   },
 
-  activeTipButtonValue(){
-    const tipButtonsGrid = document.querySelector('.buttons-grid')
-    const tipButton = tipButtonsGrid.getElementsByClassName('tip-button') 
-    let value = () => {
-      for(let i = 0; i < tipButton.length; i++) {
-        tipButton[i].addEventListener('click', () => {
-          
-          Utils.formatValue(tipButton[i].innerHTML)
-        })
+  validate() {
+    const {bill, customTip, dividedBy} = Form.getValues();
+    
+    if(bill.trim() === '' ||
+      customTip.trim() === '' ||
+      dividedBy.trim() === '') {
+        throw new Error("Please, make sure you've filled all the fields!")
       }
+
+  },
+
+  submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validate()
+    } catch (error) {
+      alert(error.message)
     }
-
-    return value
-  },
-
-  validadeFields() {
-    const {bill, dividedBy, tipCustomValue} = Form.getValues();
-    const activeButtonValue = Form.activeTipButtonValue();
-
-    console.log(activeButtonValue)
-  },
-
-  submit(e) {
-    e.preventDefault();
-
-    Form.validadeFields()
   }
 }
-
-DOM.activeTipButton()
